4d43c39311a9d86b5b047587f2740ac3c829e9e9
edonet <edonet@163.com>
2016-08-28 22:52:49 +0800

    更新【Sass】语法及补全

--- Sass/Sass_completions.py
+++ Sass/Sass_completions.py
@@ -1,21 +1,21 @@
 import sublime, sublime_plugin
 
-from CSS.completions import completions as CSS
+from Sass.completions import completions as Sass
 
 class Completions(sublime_plugin.EventListener):
 
     def on_query_completions(self, view, prefix, locations):
 
         if view.match_selector(locations[0], "source.css"):
-            return CSS.completions(self, view, prefix, locations);
+            return Sass.completions(self, view, prefix, locations);
 
         if view.match_selector(locations[0], "source.scss"):
-            return CSS.completions(self, view, prefix, locations);
+            return Sass.completions(self, view, prefix, locations);
 
         if view.match_selector(locations[0], "source.sass"):
-            return CSS.completions(self, view, prefix, locations);
+            return Sass.completions(self, view, prefix, locations);
 
         if view.match_selector(locations[0], "source.less"):
-            return CSS.completions(self, view, prefix, locations);
+            return Sass.completions(self, view, prefix, locations);
 
         return None;
